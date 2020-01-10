3ac95cb8210199105cb1250eca54b05e890eb594
Neiko2642 <Efstathiosprantalos@outlook.com>
2020-01-10 08:45:07 -0600

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','premium37.web-hosting.com');
-define('DB_USER','evlonpww_newsportal');
-define('DB_PASS' ,'gl4V=$hPU4oQ');
-define('DB_NAME','evlonpww_newsportal');
+define('DB_SERVER','');
+define('DB_USER','');
+define('DB_PASS' ,'');
+define('DB_NAME','');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
